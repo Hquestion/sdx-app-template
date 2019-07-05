3429e67076815d8529c7cac10ba7c47d0ccd5596
Xiang Dai <xiang.dai@iluvatar.ai>
2019-07-05 20:05:11 +0800

    update build.sh
    Signed-off-by: Xiang Dai <xiang.dai@iluvatar.ai>

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ init_dir()
 
 build_dict()
 {
-    docker run -d --network host -v "$ROOT_DIR":/app node:lts-alpine sh -c "npm config set registry http://192.168.20.38:4873 \
+    docker run --network host -v "$ROOT_DIR":/app node:lts-alpine sh -c "npm config set registry http://192.168.20.38:4873 \
         && cd /app \
         && npm install \
         && npm run build" || return
