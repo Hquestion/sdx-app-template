e555077486cd469e888dbde81f9671be40a66153
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-07-15 18:57:30 +0800

    首页dashboard资源加loading

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -17,9 +17,11 @@
                                         <div>
                                             <i class="iconfont iconicon-cpu-dashboard" />
                                         </div>
-                                        <div class="resource-items-content">
-                                            <div>
-                                                {{ Math.ceil(resource.cpu / 1000) }}
+                                        <div
+                                            class="resource-items-content"
+                                        >
+                                            <div v-loading="resourceLoading">
+                                                {{ resource.cpu > -1 ? Math.ceil(resource.cpu / 1000) : '-' }}
                                             </div>
                                             <div>
                                                 <span>CPU</span>（{{ $t('dashboard.core') }}）
@@ -31,8 +33,8 @@
                                             <i class="iconfont iconicon-GPU-dashboard" />
                                         </div>
                                         <div class="resource-items-content">
-                                            <div>
-                                                {{ gpuCount }}
+                                            <div v-loading="resourceLoading">
+                                                {{ gpuCount > -1 ? gpuCount : '-' }}
                                             </div>
                                             <div class="gpu-resource">
                                                 <el-select
@@ -60,8 +62,8 @@
                                             <i class="iconfont iconicon-neicun" />
                                         </div>
                                         <div class="resource-items-content">
-                                            <div>
-                                                {{ Math.ceil(resource.memory / Math.pow(1024, 3)) }}
+                                            <div v-loading="resourceLoading">
+                                                {{ resource.memory > -1 ? Math.ceil(resource.memory / Math.pow(1024, 3)) : '-' }}
                                             </div>
                                             <div>
                                                 <span>{{ $t('dashboard.memory') }}</span>（GB）
@@ -73,8 +75,8 @@
                                             <i class="iconfont iconicon-disk" />
                                         </div>
                                         <div class="resource-items-content">
-                                            <div>
-                                                {{ diskCount }}
+                                            <div v-loading="diskLoading">
+                                                {{ diskCount > -1 ? diskCount : '-' }}
                                             </div>
                                             <div>
                                                 <span>DISK</span>（GB）
@@ -251,7 +253,7 @@ export default {
             gpuValue: '',
             taskCompleted: 0,
             taskTotal: 0,
-            diskCount: 0,
+            diskCount: -1,
             orderBy: 'CPU',
             resourceType: [
                 {
@@ -291,7 +293,10 @@ export default {
             taskLoading: true,
             versionLoading: true,
             modelNameId: [],
-            taskNameId: []
+            taskNameId: [],
+            // resource loading
+            resourceLoading: true,
+            diskLoading: true
         };
     },
     components: {
@@ -304,7 +309,7 @@ export default {
     },
     computed: {
         gpuCount() {
-            let count = 0;
+            let count = -1;
             for (let i = 0; i < this.options.length; i++) {
                 if (this.options[i].value === this.gpuValue) {
                     count = this.options[i].count;
@@ -338,12 +343,15 @@ export default {
             let userId = this.$store.getters.userId;
             getUserResource(userId)
                 .then(data => {
+                    this.resourceLoading = false;
                     this.resource = data;
                     this.options = data.gpus.map(item => {
                         item.value = item.label;
                         return item;
                     });
                     this.gpuValue = data.gpus[0].label;
+                }, () => {
+                    this.resourceLoading = false;
                 });
         },
         // 任务列表
@@ -383,7 +391,10 @@ export default {
             };
             getDisk(params)
                 .then(res => {
+                    this.diskLoading = false;
                     this.diskCount = Math.ceil(res.usedBytes / Math.pow(1024, 3));
+                }, () => {
+                    this.diskLoading = false;
                 });
         },
         // 获取更多任务
