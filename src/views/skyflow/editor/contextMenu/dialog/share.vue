8f82c0eace6f3c545ab8402ed05f700f1a2e5e57
yangsheng <sheng.yang@iluvatar.ai>
2019-07-15 15:10:56 +0800

    skyflow: share dialog

--- src/views/skyflow/editor/contextMenu/dialog/share.vue
+++ src/views/skyflow/editor/contextMenu/dialog/share.vue
@@ -102,6 +102,10 @@ export default {
         sourceKind: {
             type: String,
             default: ''
+        },
+        node: {
+            type: Object,
+            required: true
         }
     },
     methods: {
