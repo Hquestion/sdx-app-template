9850e5d3c8e5bbb55a2dc5b890d68be513b71a92
xiaolei.he <xiaolei.he@iluvatar.com>
2019-09-12 16:06:45 +0800

    修改dialog引用组件库中dialog SDXTEST-171

--- vue.config.js
+++ vue.config.js
@@ -157,13 +157,8 @@ module.exports = {
             //         '/mock': '/mock/5cd04685adb0973be6a3d969/'
             //     }
             // },
-            '^/api/v1/datasource': {
-                target: 'http://10.201.0.29:5000',
-                ws: true,
-                changeOrigin: true
-            },
-            '^/api/v1/dataset': {
-                target: 'http://10.201.0.29:5000',
+            '^/api/': {
+                target: 'http://localhost:8888',
                 ws: true,
                 changeOrigin: true
             },
