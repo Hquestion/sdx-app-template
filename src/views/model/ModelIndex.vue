a2cd66f6c949d8a0f13684a82d145abd1e6dbf31
hexiaolei <1035271660@qq.com>
2019-11-19 14:43:02 +0800

    修改页面结构

---
+++ src/views/model/ModelIndex.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="model-index">
+        <ApiDetail></ApiDetail>
+    </div>
+</template>
+
+<script>
+import ApiDetail from '@sdx/view/components/model-manage-next/src/api-detail/ApiDetail';
+export default {
+    name: 'ModelIndex',
+    components: {
+        ApiDetail
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
